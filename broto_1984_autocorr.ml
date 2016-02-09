0dda23bb7142d86d822e59194f3cd6778b158692
Francois BERENGER <francois.berenger@inria.fr>
2016-02-09 14:42:28 +0100

    can score candidate versus query

--- broto_1984_autocorr.ml
+++ broto_1984_autocorr.ml
@@ -12,6 +12,7 @@ open Printf
 open Batteries
 
 module Mol2 = Mol2_parser
+module MU = My_utils
 
 let auto_correlation (atoms: Atom.atom list): (float * float) list =
   (* sort values by increasing distance: this is assumed later on *)
@@ -70,31 +71,55 @@ let b84_histo (atoms: Atom.atom list): (float * float) list =
   (* print_autocorr ac; *)
   histo 0.2 ac
 
+let truncate_histo min_len curr_len histo =
+  if curr_len > min_len then
+    List.take min_len histo
+  else
+    histo
+
 let main () =
   Log.set_log_level Log.INFO;
   Log.color_on();
   (* default option values *)
   let query_file = ref "" in
   let db_file    = ref "" in
-  let cmd_line = Arg.align [
-      "-q" , Arg.Set_string query_file, "query.mol2 query molecule";
-      "-db", Arg.Set_string db_file   , "db.mol2 database";
-    ]
+  let scores_file = ref "" in
+  let cmd_line = Arg.align
+      ["-q" , Arg.Set_string query_file , "query.mol2 query molecule";
+       "-db", Arg.Set_string db_file    , "db.mol2 database";
+       "-o" , Arg.Set_string scores_file, "out.scores score-labels file"]
   in
   Arg.parse cmd_line ignore
     (sprintf "Example: %s -q query.mol2 -db database.mol2\n" Sys.argv.(0));
-  if !query_file = "" && !db_file = "" then begin
-    Log.fatal "-q and -db are mandatory";
+  if !query_file = "" || !db_file = "" || !scores_file = "" then begin
+    Log.fatal "-q, -db and -o are all mandatory";
     exit 1
   end;
-  let query = Mol2.read_molecules !query_file in
-  let _, _, query_atoms = List.hd query in
-  let query_histo = b84_histo query_atoms in
-  print_histo query_histo
-  (* let db_molecules = Mol2.read_molecules !db_file in *)
-  (* List.iter (fun (_name, _i, atoms) -> *)
-  (*     let histo = b84_histo atoms in *)
-  (*     print_histo histo *)
-  (*   ) db_molecules *)
+  let _, _, query_atoms = 
+    match Mol2.read_molecules !query_file with
+    | [one] -> one
+    | _ -> failwith (sprintf "not one molecule in %s" !query_file)
+  in
+  let query_histo = b84_histo query_atoms |> List.map snd in (* discard distances *)
+  let query_histo_len = List.length query_histo in
+  (* print_histo query_histo; *)
+  let db_molecules = Mol2.read_molecules !db_file in
+  MU.with_out_file !scores_file (fun out ->
+      List.iter (fun (name, _i, atoms) ->
+          let histo = b84_histo atoms |> List.map snd in
+          let histo_len = List.length histo in
+          let min_len = min query_histo_len histo_len in
+          let query_histo = truncate_histo min_len query_histo_len query_histo in
+          let histo = truncate_histo min_len histo_len histo in
+          let score = MU.tversky_ref query_histo histo in
+          let is_active =
+            if BatString.starts_with name "active"
+            then 1
+            else 0
+          in
+          (* this is the format the CROC Python module reads in *)
+          fprintf out "%f %d\n" score is_active
+        ) db_molecules
+    )
 
 let () = main ()
