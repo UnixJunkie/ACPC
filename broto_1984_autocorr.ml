16cec7a7f12a51ac375b5fdd41266d2957b5a349
Francois BERENGER <francois.berenger@inria.fr>
2016-02-09 12:21:58 +0100

    added some main fun, to test

--- broto_1984_autocorr.ml
+++ broto_1984_autocorr.ml
@@ -8,7 +8,10 @@
 (* autocorrelation vectors *)
 (* they use histograms without normalization with discretization step = 0.2A *)
 
+open Printf
 open Batteries
+
+module Mol2 = Mol2_parser
 
 let auto_correlation (atoms: Atom.atom list): (float * float) list =
   (* sort values by increasing distance: this is assumed later on *)
@@ -37,15 +40,52 @@ let take_while p l =
   in
   loop [] l
 
+let fsum l =
+  let rec loop acc = function
+    | [] -> acc
+    | (_d, v) :: xs -> loop (acc +. v) xs
+  in
+  loop 0.0 l
+
 let histo dx pairs =
-  let rec loop acc to_process =
+  let rec loop curr_dx acc to_process =
     match to_process with
     | [] -> List.rev acc
     | _ ->
-      failwith "not implemented yet"
+      let values, rest = take_while (fun (d, _) -> d <= curr_dx) to_process in
+      let sum = fsum values in
+      loop (curr_dx +. dx) ((curr_dx, sum) :: acc) rest
   in
-  loop [] pairs
+  loop dx [] pairs
 
-let b84_histo (atoms: Atom.atom list): float * float list =
-  let acpc_ac = auto_correlation atoms in
-  failwith "not implemented yet"
+let b84_histo (atoms: Atom.atom list): (float * float) list =
+  histo 0.2 (auto_correlation atoms)
+
+let print_histo (hist: (float * float) list): unit =
+  List.iter (fun (d, x) -> printf "%f %f\n" d x) hist;
+  printf "\n"
+
+let main () =
+  Log.set_log_level Log.INFO;
+  Log.color_on();
+  (* default option values *)
+  let query_file = ref "" in
+  let db_file    = ref "" in
+  let cmd_line = Arg.align [
+      "-q" , Arg.Set_string query_file, "query.mol2 query molecule";
+      "-db", Arg.Set_string db_file   , "db.mol2 database";
+    ]
+  in
+  Arg.parse cmd_line ignore
+    (sprintf "Example: %s -q query.mol2 -db database.mol2\n" Sys.argv.(0));
+  if !query_file = "" && !db_file = "" then begin
+    Log.fatal "-q and -db are mandatory";
+    exit 1
+  end;
+  let db_molecules = Mol2.read_molecules !db_file in
+  List.iter (fun (_name, _i, atoms) ->
+      let histo = b84_histo atoms in
+      print_histo histo
+    ) db_molecules
+
+let () = main ()
