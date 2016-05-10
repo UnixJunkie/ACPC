dd368eead56d468cf85d9df411906d3b221b0ec1
Francois BERENGER <francois.berenger@inria.fr>
2016-05-10 15:49:59 +0200

    added dist_matrix.ml

---
+++ dist_matrix.ml
@@ -0,0 +1,28 @@
+
+module AC = Autocorr
+module HT = Hashtbl
+
+type t = (int * int, float) HT.t
+
+(* FBR: TODO use parmap *)
+
+let create autocorr_molecules: t =
+  let res = HT.create 1000 in
+  let rec loop i = function
+    | [] -> ()
+    | mol0 :: xs ->
+      List.iteri (fun j mol1 ->
+          let k = i + j + 1 in
+          let dist = AC.tanimoto mol0 mol1 in
+          HT.add res (i, k) dist
+        ) xs;
+      loop (i + 1) xs
+  in
+  loop 0 autocorr_molecules;
+  res
+
+(* return distance between molecules at indexes 'i' and 'j'
+   from the distance cache 'ht' *)
+let get (ht: t) i j: float =
+  if i = j then 0.0
+  else HT.find ht (min i j, max i j)
